9aaed166808f2a268ad0c36316d50b20be71898d
Ivan Rossi <ivan@biodec.com>
2023-12-04 11:07:18 +0100

    updating deps to last sklearn and relaxing some others

--- survtrace/__init__.py
+++ survtrace/__init__.py
@@ -1,6 +1,6 @@
 "This is a fork of the original SurvTRACE code, to be used in COMPBIOMED-UNITO projects"
 
-__version__ = "1.0.1+hive"
+__version__ = "1.0.2+hive"
 
 from .evaluate_utils import Evaluator
 from .train_utils import Trainer
